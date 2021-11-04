f01610a1536408d70175c78100b89b787b072143
ily123 <ibnovikov@gmail.com>
2021-11-04 15:04:02 -0500

    Added middleware to app.js starter.

---
+++ backend/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const csurf = require('csurf');
+const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
+
+const { environment } = require('./config');
+const isProduction = environment === 'production';
+
+// instantiate app
+const app = express();
+
+// apply moddleware
+app.use(morgan('dev')); // logging
+app.use(cookieParser()); // cookies
+app.use(express.json()); // application/json req body parser
+if (!isProduction) app.use(cors()); // enable for cors for development
+app.use(helmet({ contentSecurityPolicy: false })); // something to do with headers
+app.use(csurf({ // creates _csrf cookie & req.csrfToken() method
+  cookie: {
+    secure: isProduction,
+    sameSite: isProduction && 'Lax',
+    httpOnly: true
+  }
+}));
