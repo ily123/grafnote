527c94c9d6bdbf14b13ea1c8a33b6e50d1808e34
ily123 <ibnovikov@gmail.com>
2021-11-05 16:50:49 -0500

    Added XSRF token server for dev env.

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -8,9 +8,31 @@ const apiRouter = require('./api');
 const router = express.Router();
 router.use('/api', apiRouter);
 
-router.get('/', (req, res) => {
-  res.cookie('XSRF-TOKEN', req.csrfToken());
-  res.send('This is a test');
-});
+if (process.env.NODE_ENV === 'production') {
+  const path = require('path');
+  // Serve the frontend's index.html file at the root route
+  router.get('/', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    return res.sendFile(
+      path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+    );
+  });
+
+  // Serve the static assets in the frontend's build folder
+  router.use(express.static(path.resolve('../frontend/build')));
+
+  // Serve the frontend's index.html file at all other routes NOT starting with /api
+  router.get(/^(?!\/?api).*/, (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    return res.sendFile(
+      path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+    );
+  });
+} else {
+  router.get('/api/csrf/restore', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    return res.json({ msg: 'this is a test' });
+  });
+}
 
 module.exports = router;
