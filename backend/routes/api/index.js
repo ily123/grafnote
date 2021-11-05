86e6682b400ce0a36040df97a8a7fd37a47d65c5
ily123 <ibnovikov@gmail.com>
2021-11-04 21:18:11 -0500

    Added login validation.

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -6,7 +6,41 @@ const asyncHandler = require('express-async-handler');
 const { User } = require('../../db/models');
 const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth.js');
 
-router.post('/login', asyncHandler(async (req, res, next) => {
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
+const validateLogin = [
+  check('credential')
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage('Please provide a valid email or username.'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a password.'),
+  handleValidationErrors
+];
+
+const validateSignup = [
+  check('email')
+    .exists({ checkFalsy: true })
+    .isEmail()
+    .withMessage('Please provide a valid email.'),
+  check('username')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 4 })
+    .withMessage('Please provide a username with at least 4 characters.'),
+  check('username')
+    .not()
+    .isEmail()
+    .withMessage('Username cannot be an email.'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 6 })
+    .withMessage('Password must be 6 characters or more.'),
+  handleValidationErrors
+];
+
+router.post('/login', validateLogin, asyncHandler(async (req, res, next) => {
   const { credential, password } = req.body;
   console.log(credential);
   const user = await User.login({ credential, password });
@@ -27,12 +61,12 @@ router.delete('/logout', (req, res) => {
   return res.json({ message: 'success' });
 });
 
-router.post('/signup', asyncHandler(async (req, res) => {
+router.post('/signup', validateSignup, async (req, res) => {
   const { email, password, username } = req.body;
   const user = await User.signup({ email, username, password });
   await setTokenCookie(res, user);
   return res.json({ user });
-}));
+});
 
 router.get('/session', restoreUser, (req, res) => {
   const { user } = req;
