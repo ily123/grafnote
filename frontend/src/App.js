2ef352a456ca2b3d8f7f3af4ff457fbe6437a6cc
ily123 <ibnovikov@gmail.com>
2021-11-06 23:25:03 -0500

    Added user restore on page reload if session token present.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,17 @@
+import { useEffect } from 'react';
 import { Route, Switch, NavLink } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import LoginFormPage from './components/LoginFormPage';
+import { restoreUser } from './store/session';
 
 function App () {
   const user = useSelector(state => state.session);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(restoreUser());
+  }, [dispatch]);
+
   return (
     <div>
       <h1>Hello from App</h1>
