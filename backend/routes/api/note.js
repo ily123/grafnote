ef1d5ff6c6bb3df04c3733d4e0db7533d245a2b6
ily123 <ibnovikov@gmail.com>
2021-11-09 11:48:46 -0600

    Hooked notes router into the API router.

--- backend/routes/api/note.js
+++ backend/routes/api/note.js
@@ -1,22 +1,26 @@
 const router = require('express').Router();
 const asyncHandler = require('express-async-handler');
+const { Note } = require('../../db/models/');
 
-router.get('/note', asyncHandler(async (req, res) => {
+router.get('/', asyncHandler(async (req, res) => {
+  console.log('here I am');
+  res.json({ test: 'this is a test of the notes routing.' });
+}));
+
+router.get('/:id', asyncHandler(async (req, res) => {
 
 }));
 
-router.get('/note/:id', asyncHandler(async (req, res) => {
+router.post('/', asyncHandler(async (req, res) => {
 
 }));
 
-router.post('/note', asyncHandler(async (req, res) => {
+router.patch('/:id', asyncHandler(async (req, res) => {
 
 }));
 
-router.patch('/note/:id', asyncHandler(async (req, res) => {
+router.delete('/:id', asyncHandler(async (req, res) => {
 
 }));
 
-router.delete('/note/:id', asyncHandler(async (req, res) => {
-
-}));
+module.exports = router;
