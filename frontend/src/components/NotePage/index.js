cedb3925138e7ba76155a707f0a7346b207f729c
ily123 <ibnovikov@gmail.com>
2021-11-09 15:05:33 -0600

    Hooked up Notes page to a Route.

--- frontend/src/components/NotePage/index.js
+++ frontend/src/components/NotePage/index.js
@@ -1,3 +1,12 @@
 import './NotePage.css';
 import SideBar from './SideBar';
 import NoteEditor from './NoteEditor';
+
+export default function NotePage () {
+  return (
+    <div className='note-container'>
+      <SideBar />
+      <NoteEditor />
+    </div>
+  );
+}
