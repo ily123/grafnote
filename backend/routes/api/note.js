7224259ea16b33534222ce1d3726162e920276d7
ily123 <ibnovikov@gmail.com>
2021-11-09 11:13:01 -0600

    Added skeleton for note API routes.

---
+++ backend/routes/api/note.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+const asyncHandler = require('express-async-handler');
+
+router.get('/note', asyncHandler(async (req, res) => {
+
+}));
+
+router.get('/note/:id', asyncHandler(async (req, res) => {
+
+}));
+
+router.post('/note', asyncHandler(async (req, res) => {
+
+}));
+
+router.patch('/note/:id', asyncHandler(async (req, res) => {
+
+}));
+
+router.delete('/note/:id', asyncHandler(async (req, res) => {
+
+}));
