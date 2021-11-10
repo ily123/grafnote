13999f83182bdeb0c046cbf708957520360351e1
ily123 <ibnovikov@gmail.com>
2021-11-09 22:23:54 -0600

    Added uiwjs's react-md-editor as the text area/edit.

--- frontend/src/components/NotePage/NoteEditor.js
+++ frontend/src/components/NotePage/NoteEditor.js
@@ -1,3 +1,24 @@
-export default function NoteEditor () {
-  return <div className="note-editor-container">The editor will go here.</div>;
+import MDEditor from '@uiw/react-md-editor';
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+
+export default function NoteEditor ({ notes }) {
+  const activeNoteId = useSelector(state => state.notes.activeNoteId);
+  if (!activeNoteId) return null;
+
+  const [content, setContent] = useState('Create a new note :-)');
+  useEffect(() => {
+    const note = notes[activeNoteId];
+    setContent(note.content);
+  }, [activeNoteId]);
+
+  return (
+    <div className="note-editor-container">
+      <MDEditor
+        type="textarea"
+        value={content}
+        onChange={setContent}
+      />
+    </div>
+  );
 }
