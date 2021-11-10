f90b19fc2ddcab1a2a6ad14eb08b7ab6e2cd763f
ily123 <ibnovikov@gmail.com>
2021-11-10 14:53:50 -0600

    Added 'debounce' mechanism for note patching.
    - Used the example from this article:
      - How to debounce and throttle API calls in React by Abhishek EH
      - link: https://www.codingdeft.com/posts/react-debounce-throttle/

--- frontend/src/components/NotePage/NoteEditor.js
+++ frontend/src/components/NotePage/NoteEditor.js
@@ -1,10 +1,12 @@
 import MDEditor from '@uiw/react-md-editor';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { useSelector } from 'react-redux';
 
 export default function NoteEditor ({ notes }) {
   const activeNoteId = useSelector(state => state.notes.activeNoteId);
   const [content, setContent] = useState('Create a new note :-)');
+  const timeout = useRef();
+
   useEffect(() => {
     if (activeNoteId) {
       const note = notes[activeNoteId];
@@ -12,13 +14,24 @@ export default function NoteEditor ({ notes }) {
     }
   }, [activeNoteId]);
 
+  const handleContentChange = (content) => {
+    setContent(content);
+    clearTimeout(timeout.current);
+    timeout.current = setTimeout(() => {
+      // thunk will go here:
+      // - will save change to redux store (?)
+      // - will send PATCH request to the DB
+      console.log('i should not print more than once per second');
+    }, 1000);
+  };
+
   return (
     <div className="note-editor-container">
       <MDEditor
         className="note-editor"
         type="textarea"
         value={content}
-        onChange={setContent}
+        onChange={handleContentChange}
       />
     </div>
   );
