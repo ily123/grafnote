aa18cd5b78baff35a189a6576879be2f0d01d07e
ily123 <ibnovikov@gmail.com>
2021-11-13 18:51:46 -0600

    Removed console logs from note API routes.

--- backend/routes/api/note.js
+++ backend/routes/api/note.js
@@ -1,26 +1,22 @@
 const router = require('express').Router();
 const asyncHandler = require('express-async-handler');
-const { restoreUser } = require('../../utils/auth.js');
 const { Note } = require('../../db/models/');
 
-router.use(restoreUser);
-
 router.get('/', asyncHandler(async (req, res) => {
-  // if no user, pretend it is user with id of 1 (initial dev mocking only)
-  const userId = req.user?.id || 1;
+  const userId = req.user.id;
   const notes = await Note.findAll({ where: { userId } });
   res.json({ notes });
 }));
 
 router.get('/:id', asyncHandler(async (req, res) => {
-  const userId = req.user?.id || 1;
+  const userId = req.user.id;
   const { id } = req.params;
   const note = await Note.findOne({ where: { id, userId } });
   res.json({ note });
 }));
 
 router.post('/', asyncHandler(async (req, res) => {
-  const userId = req.user?.id || 1;
+  const userId = req.user.id;
   const { folderId, title, content } = req.body;
   const note = await Note.create({
     userId,
@@ -32,20 +28,19 @@ router.post('/', asyncHandler(async (req, res) => {
 }));
 
 router.patch('/:id', asyncHandler(async (req, res) => {
-  const userId = req.user?.id || 1;
+  const userId = req.user.id;
   const { id } = req.params;
   const { title, content, folderId } = req.body;
 
   const [_, [note]] = await Note.update(
     { title, content, folderId: +folderId === 0 ? null : folderId },
-    { where: { id }, returning: true }
+    { where: { id, userId }, returning: true }
   );
-  console.log('this is the note', note);
   res.json({ note });
 }));
 
 router.delete('/:id', asyncHandler(async (req, res) => {
-  const userId = req.user?.id || 1;
+  const userId = req.user.id;
   const { id } = req.params;
   const success = await Note.destroy({ where: { id, userId } });
   res.json({ success: !!success });
